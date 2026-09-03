6e679ca1c7fd56e224e1264f46f7dc76bfd1b2c6
Monisha1124 <moniharini07@gmail.com>
2026-09-03 10:40:21 +0530

    Add python application

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = 10
+    num2 = 20
+    total = num1 + num2
+    print(f"Number 1: {num1}")
+    print(f"Number 2: {num2}")
+    print(f"The sum of {num1} and {num2} is: {total}")
+
+if __name__ == "__main__":
+    main()
